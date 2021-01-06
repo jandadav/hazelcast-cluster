e19f53f81842462f1205836b5eb31fe058965da6
jandadav <janda.david@gmail.com>
2021-01-06 20:59:54 +0100

    integration test skelly

---
+++ integration-test/src/test/java/com/jandadav/integration/IntegrationTest.java
@@ -0,0 +1,12 @@
+package com.jandadav.integration;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IntegrationTest {
+
+    @Test
+    void deleteMe() {
+        assertTrue(true);
+    }
+}
