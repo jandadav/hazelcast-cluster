58bfdb7f2e6a66e4f46bae78439606d1e43c7f2e
jandadav <janda.david@gmail.com>
2021-01-07 20:45:00 +0100

    hazelcast-node register in discovery

--- hazelcast-node/src/main/java/com/jandadav/cache/CacheApplication.java
+++ hazelcast-node/src/main/java/com/jandadav/cache/CacheApplication.java
@@ -2,7 +2,9 @@ package com.jandadav.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CacheApplication {
     public static void main(String[] args) {
